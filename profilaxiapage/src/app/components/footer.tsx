0547e5bc0ccdeafc0092efa82c94f241126fd650
nathalia <nathaliafloresmartins4550@gmail.com>
2025-07-09 14:21:30 -0300

    adding footer to the project

---
+++ profilaxiapage/src/app/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-orange-500 text-white py-4 mt-10">
+      <div className="max-w-7xl mx-auto text-center text-sm">
+        © {new Date().getFullYear()}  Nathalia Martins MP|M5 — Todos os direitos reservados.
+      </div>
+    </footer>
+  );
+}
