f0c3cdb28ee11544d780317eb393ad16bcc601db
nathalia <nathaliafloresmartins4550@gmail.com>
2025-07-09 14:21:16 -0300

    adding header to the project

---
+++ profilaxiapage/src/app/components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="bg-orange-500 text-white px-6 py-4 shadow-md">
+      <div className="max-w-7xl mx-auto flex items-center justify-between">
+        <h1 className="text-xl font-bold">Zoonoses | Profilaxias</h1>
+      </div>
+    </header>
+  );
+}
