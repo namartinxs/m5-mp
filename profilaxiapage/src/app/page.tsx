b02c55f47f62952e015d02f6f909d29f6be452d5
nathalia <nathaliafloresmartins4550@gmail.com>
2025-07-09 14:24:27 -0300

    adding all ui routes to the central page

---
+++ profilaxiapage/src/app/page.tsx
@@ -0,0 +1,24 @@
+
+import Header from "./components/header";
+import Footer from "./components/footer";
+import LeptospiroseSection from "./components/letptoSection";
+import TratamentoChikungunyaSection from "./components/TratChikSection";
+import Zoonoses from "./components/zoonoses";
+
+export default function Home() {
+  return (
+    <>
+    <Header/>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <Zoonoses></Zoonoses>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+      <TratamentoChikungunyaSection/>
+      <LeptospiroseSection/>
+      </div>
+      
+     
+    </main>
+    <Footer/>
+    </>
+  );
+}
